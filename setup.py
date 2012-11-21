8f65157aaf87b0bde907d67c0e39bd8b1c58f0e6
Parnell Springmeyer <ixmatus@gmail.com>
2012-11-20 19:21:16 -0600

    Adding in pypi files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import sys
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+if not '2.6' <= sys.version < '3.0':
+    raise ImportError('Python version not supported')
+
+setup(name="tracebackturbo",
+      version=version,
+      description="Patched version of traceback, also dumps local and global scope vars.",
+      classifiers=["Intended Audience :: Developers",
+                   "License :: OSI Approved :: Python Software Foundation License",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
+      url="https://github.com/gopher/python-tracebackturbo",
+      license="PSF",
+      zip_safe=False,
+      packages=find_packages(),
+      include_package_data=True
+      )
